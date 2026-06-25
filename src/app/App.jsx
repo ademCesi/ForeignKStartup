c13026236499693fc96564e9284ffa423bd4fedb
Adem BENSALEM <adembensalem062@gmail.com>
2026-06-25 14:34:51 +0900

    clean: remove legalPage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import StepPage from './pages/StepPage';
-import PrivacyPage from './pages/PrivacyPage';
-import EmailPrivacyPage from './pages/EmailPrivacyPage';
 
 function App() {
   return (
@@ -11,8 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/step/:stepId" element={<StepPage />} />
-        <Route path="/privacy" element={<PrivacyPage />} />
-        <Route path="/emailPrivacy" element={<EmailPrivacyPage />} />
       </Routes>
     </BrowserRouter>
   );
